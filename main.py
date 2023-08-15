f3de9c327b5a1e22d17e0320acaaf51f88257e61
ilginceliksoz <141743744+ilginceliksoz@users.noreply.github.com>
2023-08-15 14:24:30 +0000

    firstcommmit

---
+++ main.py
@@ -0,0 +1,14 @@
+name= "Mustafa"
+surname= "Yıldız"
+surnames= ["Yıldız", "Aksoy", "Demir"]
+for soyad in surnames:
+print(soyad)
+if surname == soyad:
+    
+if surname == "Yıldız":
+    print(True)
+else:
+    print(False)
+print(name[0])
+print(surnames[0])
+print("")
